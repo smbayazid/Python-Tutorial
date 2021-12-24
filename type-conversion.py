8ac077e9f139dea3dc4a65020412bc9c65356f36
Sufi Bayazid <47542155+smbayazid@users.noreply.github.com>
2021-12-25 04:36:31 +0600

    variable and type-conversion adding

---
+++ type-conversion.py
@@ -0,0 +1,7 @@
+# the aim is to convert the type of variable and manipulating it
+
+weightInKG = input('Enter your weight in KG: ')
+
+weightInLbs = int(weightInKG) * 2.2046
+
+print('You are', weightInLbs, 'lbs.')
---
+++ variables.py
@@ -0,0 +1,6 @@
+name = input('Enter Name: ')
+age = input('Enter Age: ')
+
+print('============================')
+print(name, 'is', age, 'years old.')
+print('============================')
