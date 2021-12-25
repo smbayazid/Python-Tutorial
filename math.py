4bf6acbd79d81daa57d1597da5823744ea7e2d9b
Sufi Bayazid <47542155+smbayazid@users.noreply.github.com>
2021-12-25 10:39:28 +0600

    math and string basics

---
+++ math.py
@@ -0,0 +1,12 @@
+x = 10
+y = 4
+
+print(f'{x} + {y} = ', x + y)
+print(f'{x} - {y} = ', x - y)
+print(f'{x} X {y} = ', x * y)
+print(f'{x} / {y} = ', x / y)
+print(f'{x} % {y} = ', x % y)
+print(f'{x} ^ {y} = ', x ** y)
+
+# Precedence
+print(10 / (5 - 3) * 2 ** 2 + 7)
---
+++ string.py
@@ -0,0 +1,20 @@
+# Concatenate
+greeting = 'Hello'
+text = 'How are you?'
+
+print(greeting + text)
+
+# Formatted String
+firstName = 'Sufi'
+lastName = 'Bayazid'
+
+message = f'{firstName} [{lastName}] is a Python coder...'
+
+print(message)
+
+# Functions and Methods
+str = 'I am learning python.'
+print(len(str))
+print(str.upper())
+print(str.lower())
+print(str.replace(' am', "'m"))
